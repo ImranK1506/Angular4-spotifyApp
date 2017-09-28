222f5dbe129482b74ef6ab50e5bfe82cd38065c4
Imran Khan <m_imran_khan@me.com>
2017-09-28 19:02:25 +0200

    add search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule }    from '@angular/platform-browser';
-import { NgModule }         from '@angular/core';
-import { RouterModule, Routes }     from '@angular/router';
-import { FormsModule }      from '@angular/forms';
+import { BrowserModule }          from '@angular/platform-browser';
+import { NgModule }               from '@angular/core';
+import { RouterModule, Routes }   from '@angular/router';
+import { FormsModule }            from '@angular/forms';
 
-import { AppComponent }     from './app.component';
-import { SearchComponent }  from './components/search/search.component';
-import { NavbarComponent }  from './components/navbar/navbar.component';
-import { AboutComponent }   from './components/about/about.component';
+import { AppComponent }           from './app.component';
+import { SearchComponent }        from './components/search/search.component';
+import { NavbarComponent }        from './components/navbar/navbar.component';
+import { AboutComponent }         from './components/about/about.component';
 
 const routes: Routes = [
   {
